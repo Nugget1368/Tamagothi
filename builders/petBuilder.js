51e54a348b471cf36a5eef8cbc2bb199822fb5d8
Nugget1368 <rneg1@hotmail.com>
2025-03-19 12:13:49 +0100

    Added Card-styling

--- builders/petBuilder.js
+++ builders/petBuilder.js
@@ -1,8 +1,10 @@
 export const buildPet = (pet) => {
     let article = document.createElement("article");
-    article.classList.add("tamagothi");
+    article.classList.add("tamagotchi");
+    let picture = document.createElement("picture");
     let img = document.createElement("img");
     img.src = pet.img;
+    picture.append(img);
     let name = document.createElement("label");
     name.textContent = `Name: ${pet.name}`;
     let energy = document.createElement("label");
@@ -10,7 +12,9 @@ export const buildPet = (pet) => {
     let fullness = document.createElement("label");
     fullness.textContent = `Fullness: ${pet.fullness}`;
     let div = document.createElement("div");
-    div.append(img, name, energy, fullness);
+    let content = document.createElement("div");
+    content.append(name, energy, fullness);
+    div.append(picture, content);
     article.append(div);
     let section = document.querySelector("main section");
     section.append(article);
--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 import { Cat, Fox, Panda, Toilet } from "./classes/Pet.js"
+import { buildPet } from "./builders/petBuilder.js";
+
+let pets = [];
+let cat = new Cat("Mira Mus");
+pets.push(cat);
+pets.forEach((pet) =>{
+    buildPet(pet);
+});
