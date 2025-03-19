43b640411ea95a26a173881b312b10c386dc7999
Nugget1368 <rneg1@hotmail.com>
2025-03-19 12:14:41 +0100

    Build pets from array

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ import { buildPet } from "./builders/petBuilder.js";
 let pets = [];
 let cat = new Cat("Mira Mus");
 pets.push(cat);
-pets.forEach((pet) =>{
+pets.forEach((pet) => {
     buildPet(pet);
 });
