4c19af5d94c2ffad8fc99b4eba8ed0fc74dda9f1
Nugget1368 <rneg1@hotmail.com>
2025-03-20 15:21:07 +0100

    Game Class and Functionality Add-pet

---
+++ classes/Game.js
@@ -0,0 +1,41 @@
+import { Cat, Fox, Panda, Toilet } from "./Pet.js"
+import { buildPet } from "../builders/petBuilder.js";
+
+export class Game {
+    static startGame = (pets = []) => {
+        // Build stored pets
+        if(pets.length > 0){
+            pets.forEach((pet) => {
+                buildPet(pet);
+            });
+        };
+        // Add eventlisteners
+        let modal = document.querySelector("dialog[data-modal]");
+        let openModalBtn = document.querySelector("button[data-open-modal]");
+        openModalBtn.addEventListener("click", () => modal.showModal());
+        let closeModalBtn = document.querySelector("[data-close-modal]");
+        closeModalBtn.addEventListener("click", () => modal.close());
+        let confirmBtn = document.querySelector("dialog[data-modal] button.primary-btn");
+        confirmBtn.addEventListener("click", () => {
+            let pet = Game.addPet();
+            pets.push(pet);
+            buildPet(pet);
+            modal.close();
+        });
+    }
+
+    static addPet = () => {
+        let name = document.querySelector("#name").value;
+        let type = document.querySelector("#type").value;
+        let pet = {};
+        if (type == "Cat")
+            pet = new Cat(name);
+        else if (type == "Fox")
+            pet = new Fox(name);
+        else if (type == "Panda")
+            pet = new Panda(name);
+        else if (type == "Toilet")
+            pet = new Toilet(name);
+        return pet;
+    }
+}
