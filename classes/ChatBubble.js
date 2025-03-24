42ecb7aa5cbf6cc9d7acb8b9e1ab65432697444c
Nugget1368 <rneg1@hotmail.com>
2025-03-24 10:59:01 +0100

    Set Timeout for closing chatBubble after 3s.

--- classes/ChatBubble.js
+++ classes/ChatBubble.js
@@ -7,11 +7,13 @@ export class chatBubble{
 
     static closeBubble = (id) => {
         let bubble = document.querySelector(`.chat-bubble#${id}`);
+        bubble.innerHTML = ``;
         bubble.classList.add("hide");
     }
 
     static displayText = (id, text = "") =>{
         let bubble = this.openBubble(id);
         bubble.innerHTML = `<p>${text}</p>`;
+        setTimeout(() => this.closeBubble(id), 3000);
     }
 }
